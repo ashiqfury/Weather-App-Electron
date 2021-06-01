6d9b28a721ebd96e0984f1dfa5b459dae2e81a24
Fury <ashiqasraf07@gmail.com>
2021-06-01 06:20:22 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const input = document.querySelector(".input-fields input"),
+btn = document.querySelector(".input-fields button"),
+cityName = document.querySelector("#name"),
+temp = document.querySelector("#temp"),
+desc = document.querySelector("#desc"),
+wind = document.querySelector("#wind"),
+country = document.querySelector("#country"),
+img = document.querySelector("img");
+
+const API_KEY = "3d8fcfb5b8561690a8ad5272d7faec63";
+
+btn.onclick = () => {
+    const city = input.value;    
+    
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+
+        console.log(data);
+
+        cityName.innerHTML = data.name;
+        country.innerHTML = data.sys.country;
+
+        var iconId = data.weather[0].icon;
+        img.setAttribute("src", `icons/${iconId}.png`);
+        img.classList.remove("display");
+
+        var kelvin = data.main.temp;
+        var celsius = kelvin - 273.15;
+        temp.innerHTML = `${celsius.toFixed(2)} C`;
+        
+        desc.innerHTML = data.weather[0].description;
+        wind.innerHTML = `Wind : ${data.wind.speed}`;
+
+    })
+    .catch((err) => {
+        cityName.innerHTML = "No City Found";
+    });
+}
